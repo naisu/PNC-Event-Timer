9ecc777169c1c869b1be3a3a7188361e815c6f51
naisu <pgomezchan@gmail.com>
2023-03-07 22:34:52 -0800

    Update to reflect Inverted Mordent Resonance event

--- js/script.js
+++ js/script.js
@@ -93,22 +93,23 @@ function timerRemoveElements(id, itemId) {
 
 //Hours +8 to account for UTC-8
 
-// TIMER 0 (ENDS: 2023/3/7 18:30:00) [Sweet Secrets, Quenching Operation, Python, Vee]
-setInterval(function () { countDown('#days-t0', '#hours-t0', '#minutes-t0', '#seconds-t0', 'March 8, 2023 02:30:00 UTC+00:00', '.t0', '.t0-item') }, 1000);
+// TIMER 0 (ENDS: 2023/3/28 19:30:00) [Nascita, Nanaka]
+setInterval(function () { countDown('#days-t0', '#hours-t0', '#minutes-t0', '#seconds-t0', 'March 28, 2023 03:30:00 UTC+00:00', '.t0', '.t0-item') }, 1000);
 
 // TIMER 1 (ENDS: 2023/03/20 05:00:00 [Magrasea Battle Pass])
 setInterval(function () { countDown('#days-t1', '#hours-t1', '#minutes-t1', '#seconds-t1', 'March 20, 2023 13:00:00 UTC+00:00', '.t1', '.t1-item') }, 1000);
 
-// TIMER 3 (STARTS: 2023/03/13 05:00:00 [Exception Protocol Season 5]) 
+// TIMER 3 (ENDS: 2023/03/13 05:00:00 [Exception Protocol Season 5]) 
 setInterval(function () { countDown('#days-t3', '#hours-t3', '#minutes-t3', '#seconds-t3', 'March 13, 2023 13:00:00 UTC+00:00', '.t3', '.t3-item') }, 1000);
 
+// TIMER 4 (ENDS: 2023/03/15 05:00:00 [Luxuriant Springtide]) 
+setInterval(function () { countDown('#days-t4', '#hours-t4', '#minutes-t4', '#seconds-t4', 'March 15, 2023 13:00:00 UTC+00:00', '.t4', '.t4-item') }, 1000);
 
+// TIMER 5 (ENDS: 2023/03/21 19:30:00 [Puzzle])
+setInterval(function () { countDown('#days-t5', '#hours-t5', '#minutes-t5', '#seconds-t5', 'March 21, 2023 03:30:00 UTC+00:00', '.t5', '.t5-item') }, 1000);
 
-// TIMER 5 (ENDS: 2023/03/06 05:00:00 [2x Algo])
-setInterval(function () { countDown('#days-t5', '#hours-t5', '#minutes-t5', '#seconds-t5', 'March 6, 2023 13:00:00 UTC+00:00', '.t5', '.t5-item') }, 1000);
-
-// TIMER 6 (ENDS: 2023/03/05 05:00:00 [Luxuriant Springtide])
-setInterval(function () { countDown('#days-t6', '#hours-t6', '#minutes-t6', '#seconds-t6', 'March 5, 2023 13:00:00 UTC+00:00', '.t6', '.t6-item') }, 1000);
+// TIMER 6 (ENDS: 2023/04/04 18:29:00 [Otium, Burbank Furniture])
+setInterval(function () { countDown('#days-t6', '#hours-t6', '#minutes-t6', '#seconds-t6', 'April 5, 2023 03:30:00 UTC+00:00', '.t6', '.t6-item') }, 1000);
 
 
 
