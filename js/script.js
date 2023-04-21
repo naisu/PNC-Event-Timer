91c0732919a7a69d53f549b1e49f3a6d57ee284b
naisu <pgomezchan@gmail.com>
2023-04-20 22:26:46 -0700

    Update to reflect current events

--- js/script.js
+++ js/script.js
@@ -93,20 +93,21 @@ function timerRemoveElements(id, itemId) {
 
 //Hours +8 to account for UTC-8
 
-// TIMER 0 (ENDS: 2023/5/2 18:30:00) [Call of Study, Furniture Set, Helix Event End, Helix]
+// TIMER 0 (ENDS: 2023/5/2 18:30:00) [Call of Study, Furniture Set, Helix Event End, Helix, Evelyn, De lacey]
 setInterval(function () { countDown('#days-t0', '#hours-t0', '#minutes-t0', '#seconds-t0', 'May 3, 2023 02:30:00 UTC+00:00', '.t0', '.t0-item') }, 1000);
 
 // TIMER 1 (ENDS: 2023/05/15 05:00:00 [Magrasea Battle Pass])
 setInterval(function () { countDown('#days-t1', '#hours-t1', '#minutes-t1', '#seconds-t1', 'May 15, 2023 12:00:00 UTC+00:00', '.t1', '.t1-item') }, 1000);
 
+// TIMER 2 (ENDS: 2023/04/27 05:00:00 [2x Vuln]) 
+setInterval(function () { countDown('#days-t2', '#hours-t2', '#minutes-t2', '#seconds-t2', 'April 27, 2023 12:00:00 UTC+00:00', '.t2', '.t2-item') }, 1000);
+
 // TIMER 3 (ENDS: 2023/04/24 05:00:00 [Exception Protocol]) 
 setInterval(function () { countDown('#days-t3', '#hours-t3', '#minutes-t3', '#seconds-t3', 'April 24, 2023 12:00:00 UTC+00:00', '.t3', '.t3-item') }, 1000);
 
-// TIMER 6 (ENDS: 2023/05/02 18:30:00 [])
-setInterval(function () { countDown('#days-t6', '#hours-t6', '#minutes-t6', '#seconds-t6', 'May 3, 2023 02:30:00 UTC+00:00', '.t6', '.t6-item') }, 1000);
+// TIMER 4 (ENDS: 2023/05/16 19:30:00 [Roar in the New]) 
+setInterval(function () { countDown('#days-t4', '#hours-t4', '#minutes-t4', '#seconds-t4', 'May 17, 2023 02:30:00 UTC+00:00', '.t4', '.t4-item') }, 1000);
 
-// TIMER 8 (ENDS: 2023/04/18 18:30:00 [Banksy, Kuro])
-setInterval(function () { countDown('#days-t8', '#hours-t8', '#minutes-t8', '#seconds-t8', 'April 19, 2023 02:30:00 UTC+00:00', '.t8', '.t8-item') }, 1000);
 
 // TIMER FOR SERVER / RESET
 setInterval(function () { countReset(".sr-hours", ".sr-minutes", ".sr-seconds") }, 1000)
