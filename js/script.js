2f716f4218f092d3d962b785948f04f996b468ca
naisu <pgomezchan@gmail.com>
2023-05-02 21:52:59 -0700

    Update to reflect Magnhilda event and banner

--- js/script.js
+++ js/script.js
@@ -93,8 +93,8 @@ function timerRemoveElements(id, itemId) {
 
 //Hours +8 to account for UTC-8
 
-// TIMER 0 (ENDS: 2023/5/2 18:30:00) [Call of Study, Furniture Set, Helix Event End, Helix, Evelyn, De lacey]
-setInterval(function () { countDown('#days-t0', '#hours-t0', '#minutes-t0', '#seconds-t0', 'May 3, 2023 02:30:00 UTC+00:00', '.t0', '.t0-item') }, 1000);
+// TIMER 0 (ENDS: 2023/5/23 18:30:00) [Eager Fist, Dawning Fray, Magnhilda Banner]
+setInterval(function () { countDown('#days-t0', '#hours-t0', '#minutes-t0', '#seconds-t0', 'May 24, 2023 02:30:00 UTC+00:00', '.t0', '.t0-item') }, 1000);
 
 // TIMER 1 (ENDS: 2023/05/15 05:00:00 [Magrasea Battle Pass])
 setInterval(function () { countDown('#days-t1', '#hours-t1', '#minutes-t1', '#seconds-t1', 'May 15, 2023 12:00:00 UTC+00:00', '.t1', '.t1-item') }, 1000);
@@ -107,6 +107,12 @@ setInterval(function () { countDown('#days-t3', '#hours-t3', '#minutes-t3', '#se
 
 // TIMER 4 (ENDS: 2023/05/16 19:30:00 [Roar in the New]) 
 setInterval(function () { countDown('#days-t4', '#hours-t4', '#minutes-t4', '#seconds-t4', 'May 17, 2023 02:30:00 UTC+00:00', '.t4', '.t4-item') }, 1000);
+
+// TIMER 5 (ENDS: 2023/5/16 18:30:00) [Hubble, Gin Banner]
+setInterval(function () { countDown('#days-t5', '#hours-t5', '#minutes-t5', '#seconds-t5', 'May 17, 2023 02:30:00 UTC+00:00', '.t5', '.t5-item') }, 1000);
+
+// TIMER 6 (ENDS: 2023/5/30 18:30:00) [Classical Repertoire]
+setInterval(function () { countDown('#days-t6', '#hours-t6', '#minutes-t6', '#seconds-t6', 'May 31, 2023 02:30:00 UTC+00:00', '.t6', '.t6-item') }, 1000);
 
 
 // TIMER FOR SERVER / RESET
