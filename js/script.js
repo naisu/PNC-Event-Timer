988af4d5fead99c5a9ec9f745a8fdcfc898d026c
naisu <pgomezchan@gmail.com>
2023-07-12 10:24:35 -0700

    Update to relfect midnight machination event

--- js/script.js
+++ js/script.js
@@ -93,11 +93,11 @@ function timerRemoveElements(id, itemId) {
 
 //Hours +8 to account for UTC-8
 
-// TIMER 0 (ENDS: 2023/6/20 18:30:00) [Gastronomic Journey, Millau, Nascita, Python]
-setInterval(function () { countDown('#days-t0', '#hours-t0', '#minutes-t0', '#seconds-t0', 'July 12, 2023 02:30:00 UTC+00:00', '.t0', '.t0-item') }, 1000);
+// TIMER 0 (ENDS: 2023/8/01 18:30:00) [Midnight Machinations]
+setInterval(function () { countDown('#days-t0', '#hours-t0', '#minutes-t0', '#seconds-t0', 'August 02, 2023 02:30:00 UTC+00:00', '.t0', '.t0-item') }, 1000);
 
-// TIMER 1 (ENDS: 2023/06/26 05:00:00 [Theater Retro])
-setInterval(function () { countDown('#days-t1', '#hours-t1', '#minutes-t1', '#seconds-t1', 'July 26, 2023 02:30:00 UTC+00:00', '.t1', '.t1-item') }, 1000);
+// TIMER 1 (ENDS: 2023/07/26 19:30:00 [Theater Retro, Evenlyn, Banxsy])
+setInterval(function () { countDown('#days-t1', '#hours-t1', '#minutes-t1', '#seconds-t1', 'July 26, 2023 03:30:00 UTC+00:00', '.t1', '.t1-item') }, 1000);
 
 // TIMER 2 (ENDS: 2023/07/26 05:00:00 [Wishing Starbridge Workshop]) 
 setInterval(function () { countDown('#days-t2', '#hours-t2', '#minutes-t2', '#seconds-t2', 'July 26, 2023 13:00:00 UTC+00:00', '.t2', '.t2-item') }, 1000);
@@ -105,8 +105,12 @@ setInterval(function () { countDown('#days-t2', '#hours-t2', '#minutes-t2', '#se
 // TIMER 3 (ENDS: 2023/07/03 05:00:00 [Exception Protocol]) 
 setInterval(function () { countDown('#days-t3', '#hours-t3', '#minutes-t3', '#seconds-t3', 'July 17, 2023 13:00:00 UTC+00:00', '.t3', '.t3-item') }, 1000);
 
-// TIMER 4 (ENDS: 2023/07/10 05:00:00 [Magrasea]) 
-setInterval(function () { countDown('#days-t4', '#hours-t4', '#minutes-t4', '#seconds-t4', 'July 10, 2023 13:00:00 UTC+00:00', '.t4', '.t4-item') }, 1000);
+// TIMER 4 (ENDS: 2023/08/07 05:00:00 [Magrasea]) 
+setInterval(function () { countDown('#days-t4', '#hours-t4', '#minutes-t4', '#seconds-t4', 'August 07, 2023 13:00:00 UTC+00:00', '.t4', '.t4-item') }, 1000);
+
+// TIMER 5 (ENDS: 2023/08/08 18:30:00 [Seaside Concerto, Ocean Dancer]) 
+setInterval(function () { countDown('#days-t5', '#hours-t5', '#minutes-t5', '#seconds-t5', 'August 09, 2023 02:30:00 UTC+00:00', '.t5', '.t5-item') }, 1000);
+
 
 // TIMER FOR SERVER / RESET
 setInterval(function () { countReset(".sr-hours", ".sr-minutes", ".sr-seconds") }, 1000)
